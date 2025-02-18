8d6d98d75f609bdee64a17b0042bff6e3f342a04
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2025-02-18 13:39:00 -0700

    feat: Create signup and login activity class files

---
+++ code/app/src/main/java/com/hamidat/nullpointersapp/LoginActivity.java
@@ -0,0 +1,6 @@
+package com.hamidat.nullpointersapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ code/app/src/main/java/com/hamidat/nullpointersapp/SignUpActivity.java
@@ -0,0 +1,6 @@
+package com.hamidat.nullpointersapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpActivity extends AppCompatActivity {
+}
