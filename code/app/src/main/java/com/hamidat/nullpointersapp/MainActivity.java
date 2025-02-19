d95a42f4dceb00b6c0dba077a9208ba4a267974e
Salim Soufi <86317281+ssoufii@users.noreply.github.com>
2025-02-19 12:39:54 -0700

    homescreen borderline done

--- code/app/src/main/java/com/hamidat/nullpointersapp/MainActivity.java
+++ code/app/src/main/java/com/hamidat/nullpointersapp/MainActivity.java
@@ -19,22 +19,22 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // For testing, a button to go to the profile view
-        Button openProfileViewButton = findViewById(R.id.openProfileViewButton);
-
-        openProfileViewButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(intent); // Launch ProfileActivity
-            }
-        });
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        // For testing, a button to go to the profile view
+//        Button openProfileViewButton = findViewById(R.id.openProfileViewButton);
+//
+//        openProfileViewButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
+//                startActivity(intent); // Launch ProfileActivity
+//            }
+//        });
+//
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 
 }
