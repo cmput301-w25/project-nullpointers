0a66960ebf5ec1ea29b136d43f582c09fffdf141
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2025-02-19 13:14:07 -0700

    feat: Add moodify logo to signup and login

--- code/app/src/main/java/com/hamidat/nullpointersapp/utils/AuthHelpers.java
+++ code/app/src/main/java/com/hamidat/nullpointersapp/utils/AuthHelpers.java
@@ -1,14 +1,21 @@
 package com.hamidat.nullpointersapp.utils;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
 import android.content.Context;
+import android.content.Intent;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.hamidat.nullpointersapp.LoginActivity;
+import com.hamidat.nullpointersapp.MainActivity;
+import com.hamidat.nullpointersapp.SignUpActivity;
 
 public class AuthHelpers {
     public static boolean validateNoEmptyFields(Context context, EditText... textFieldsToCheck) {
         for (EditText editTextField : textFieldsToCheck){
             if (editTextField.getText().toString().trim().isEmpty()){
-                Toast.makeText(context, "Please fill in all fields", Toast.LENGTH_SHORT).show();
+                giveAuthNotification(context, "Please fill in all fields");
                 return false;
             }
         }
@@ -26,9 +33,13 @@ public class AuthHelpers {
     public static boolean addNewUserToDB (Context context, String newUserUsername, String newUserPassword) {
         // TODO - connect to the DB and add adding a new user
         // Give a response message that they're registered
-        Toast.makeText(context, "You have been successfully registered", Toast.LENGTH_SHORT).show();
-
+        giveAuthNotification(context, "You have been successfully registered");
         // For testing, just returning true
         return true;
+    }
+
+    // Create a styles toast message for visual feedback
+    public static void giveAuthNotification(Context context, String notificationMessage) {
+        Toast.makeText(context, notificationMessage, Toast.LENGTH_SHORT).show();
     }
 }
