3f3e53a693936c343544435f87b99500a0d0090f
Salim Soufi <86317281+ssoufii@users.noreply.github.com>
2025-02-20 10:14:34 -0700

    image icon made into button

--- code/app/src/main/java/com/hamidat/nullpointersapp/MainActivity.java
+++ code/app/src/main/java/com/hamidat/nullpointersapp/MainActivity.java
@@ -1,40 +1,23 @@
 package com.hamidat.nullpointersapp;
-
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.ImageButton;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-//        // For testing, a button to go to the profile view
-//        Button openProfileViewButton = findViewById(R.id.openProfileViewButton);
-//
-//        openProfileViewButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(MainActivity.this, ProfileActivity.class);
-//                startActivity(intent); // Launch ProfileActivity
-//            }
-//        });
-//
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        ImageButton btnAddMood = findViewById(R.id.btnAddMood);
+        btnAddMood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show a Toast popup when clicked
+                Toast.makeText(MainActivity.this, "Done", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
 }
