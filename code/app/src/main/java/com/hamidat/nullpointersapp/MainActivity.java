99e28214929d42c48600876e48a0e9e6c50aa0d8
Shahab Ibrahimgit config --global user.name Shahab <ishahab84@gmail.com>
2025-02-21 09:28:27 -0700

    changes before moving to develop

--- code/app/src/main/java/com/hamidat/nullpointersapp/MainActivity.java
+++ code/app/src/main/java/com/hamidat/nullpointersapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.hamidat.nullpointersapp;
 
 import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +22,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
     }
 }
