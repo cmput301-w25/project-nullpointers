872417c523536df1c3acd6d30d704a9804cfabd2
Hamidat Bello <107907514+hamidatb@users.noreply.github.com>
2025-03-08 10:32:55 -0700

    feat: add skip auth through logging in as the gest user

--- code/app/src/main/java/com/hamidat/nullpointersapp/authFragments/LoginFragment.java
+++ code/app/src/main/java/com/hamidat/nullpointersapp/authFragments/LoginFragment.java
@@ -52,6 +52,39 @@ public class LoginFragment extends Fragment {
         final Button btnSkipAuth = rootView.findViewById(R.id.btnSkipAuthForDemo);
         final TextView tvRegisterNow = rootView.findViewById(R.id.tvRegisterNow);
 
+        btnSkipAuth.setOnClickListener( v -> {
+            giveAuthNotification(requireContext(), "Request to login as a guest user received");
+
+            // if they're not using auth, just log them in as the guest user
+            final String loginUsername = "guestUser";
+            final String loginPassword = "123";
+
+            FirestoreHelper firestoreHelper = new FirestoreHelper();
+            firestoreHelper.getUserByUsername(loginUsername, new FirestoreHelper.FirestoreCallback() {
+                @Override
+                public void onSuccess(Object result) {
+                    Map<String, Object> userData = (Map<String, Object>) result;
+                    String storedPassword = (String) userData.get("password");
+                    String userId = (String) userData.get("userId"); // The document ID
+
+                    if (storedPassword != null && storedPassword.equals(loginPassword)) {
+                        final Intent intent = new Intent(requireActivity(), MainActivity.class);
+                        // Pass the userId of the currently logged in user as an extra
+                        intent.putExtra("USER_ID", userId);
+                        startActivity(intent);
+                        requireActivity().finish();
+                    } else {
+                        giveAuthNotification(requireContext(), "Incorrect password");
+                    }
+                }
+                @Override
+                public void onFailure(Exception e) {
+                    giveAuthNotification(requireContext(), "Login failed: " + e.getMessage());
+                }
+            });
+        });
+
+
         btnLogin.setOnClickListener(v -> {
             giveAuthNotification(requireContext(), "Request to login received");
 
