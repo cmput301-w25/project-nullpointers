b2dd92595dd571b38374516266b1da97369ab504
bigriceman0128 <112298940+ardenmonaghan@users.noreply.github.com>
2025-03-15 12:08:54 -0600

    Feature: Adding xml for filtering moods, need to work on the fragment display

---
+++ code/app/src/main/java/com/hamidat/nullpointersapp/utils/homeFeedUtils/filterMoodsFragment.java
@@ -0,0 +1,10 @@
+package com.hamidat.nullpointersapp.utils.homeFeedUtils;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class filterMoodsFragment extends BottomSheetDialogFragment {
+
+    private String moodDescription;
+    private String filterMood;
+
+}
